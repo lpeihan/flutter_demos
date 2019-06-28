0c81c82ee5c7ba7f4ee74a598d492713b1d7bf5e
李培汉 <lipeihan@chandashi.com>
2019-06-28 14:18:11 +0800

    feat: stack_demo

--- lib/demo/just_demo.dart
+++ lib/demo/just_demo.dart
@@ -4,16 +4,59 @@ class JustDemo extends StatelessWidget {
   const JustDemo({Key key}) : super(key: key);
 
   List<Widget> _buildList() {
-
+    return List.generate(50, (int index) {
+      return Text('Item $index');
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('just'),
-      ),
-      body: Container()
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+          appBar: AppBar(
+            title: Text('just'),
+          ),
+          body: Stack(
+            children: <Widget>[
+              Container(
+                color: Colors.blueAccent,
+                child: ListView(
+                  children: this._buildList()
+                ),
+              ),
+              Positioned(
+                top: 20.0,
+                right: 20.0,
+                child: SizedBox(
+                  width: 20.0,
+                  height: 20.0,
+                  child: Container(
+                    color: Colors.green
+                  ),
+                ),
+              )
+            ],
+          ),
+          bottomNavigationBar: BottomAppBar(
+            child: Container(
+              alignment: Alignment.center,
+              height: 50.0,
+              color: Colors.red,
+              child: Text('hello world'),
+            ),
+          )
+        ),
+        Positioned(
+          child: Container(
+            width: 50.0,
+            height: 30.0,
+            color: Colors.orange
+          ),
+          top: 20.0,
+          right: 20.0
+        )
+      ],
     );
   }
 }
